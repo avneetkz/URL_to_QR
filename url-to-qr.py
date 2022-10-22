d61606b0aeb74ebb558f42bcdb38af7416dbeafd
Avneet kaur <avneetzandu2001@gmail.com>
2022-10-22 14:40:51 +0530

    Update url-to-qr.py

--- url-to-qr.py
+++ url-to-qr.py
@@ -5,6 +5,7 @@ import image
 
 # define function to create QR codeg
 def url_to_qr(data):
+    
     # calling QRCode attribute from qrcode module 
     # defining the size of code in paramenters
     qr= qrcode.QRCode( version= 10,  
@@ -12,8 +13,10 @@ def url_to_qr(data):
                        border= 30 )
     
     qr.add_data(data)
+    
     # fit automatically detects the size if no size was given
     qr.make(fit= True)
+    
     # converting qr to image
     img= qr.make_image( fill= 'white', back_colour= 'black')
     img.save('qr.png')
@@ -21,6 +24,7 @@ def url_to_qr(data):
 # driver code
 if __name__ == "__main__":
     url= input("Enter the url : ")
+    
     # function call 
     url_to_qr(url)
 
